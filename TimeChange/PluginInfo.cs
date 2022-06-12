e572f27c1b78ec84bfeaab6df1ecf917c16c06c2
rass1010 <62247959+rass1010@users.noreply.github.com>
2022-06-12 20:35:51 +0200

    Add files via upload

---
+++ TimeChange/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TimeChange
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.rass1010.gorillatag.timechange";
+        public const string Name = "TimeChange";
+        public const string Version = "1.0.0";
+    }
+}
